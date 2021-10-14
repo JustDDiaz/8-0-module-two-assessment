dc79a1e52b4cb1bee6be4066c770566b30d12601
justin <justin.d.diaz@gmail.com>
2021-10-14 16:57:42 -0400

    i hit a wall

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ fetch("https://ghibliapi.herokuapp.com/films/")
       select.append(option);
     });
   });
+
+document.querySelector("#drop-down").addEventListener("change", (event) => {
+  fetch("https://ghibliapi.herokuapp.com/films/")
+    .then((response) => response.json())
+    .then((moves) => {
+      moves.forEach((move) => {
+        if (event.target.value === move.title.value) {
+          const drop = document.querySelector("drop-down");
+          const name = move.title.value;
+          const year = move.relase_fate.value;
+          const info = move.discription.value;
+          const h2 = document.createElement("h2");
+          const p1 = document.createElement("p1");
+          const p2 = document.createElement("p2");
+          drop.append(h2);
+          drop.append(p1);
+          drop.append(p2);
+          h2.textContent = name;
+          p1.textContent = year;
+          p2.textContent = info;
+          event.preventDefault();
+        }
+      });
+    });
+});
+
+document.querySelector("#review form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const review = document.querySelector("#review");
+  const text = document.querySelector("#review-text");
+  console.log(text);
+});
